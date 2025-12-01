c14c99255ac3a3103ea655f080b46af26408bb4a
KARKUVEL17 <karkuvel25082003@gmail.com>
2025-12-01 15:19:51 +0530

    tttttt "Merge pull request #2 from KARKUVEL17/feature-"
    This reverts commit 10a4e85833604b92d506e85116a3f84caa854011, reversing
    changes made to e3906d302d4ea2a4e1176e10c93eafdef8da2653.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,10 +2,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        int i=0;
-        while(i<5){
-            System.out.println(i);
-            i++;
         for(int i=0;i<5;i++){
             System.out.println(i);
         }
